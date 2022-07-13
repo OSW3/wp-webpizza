18c98619d4ff1a05c34e58e06270e5b652ced17f
Arnaud BODEL <contact@osw3.net>
2022-07-13 15:19:25 +0200

    -

---
+++ footer.php
@@ -0,0 +1,54 @@
+<!-- ======================================================================= -->
+<!-- FOOTER START                                                            -->
+<!-- ======================================================================= -->
+        </div> <!-- end .container -->
+    </div><!-- end .main-container -->
+
+    <!-- Main Footer -->
+    <footer class="main-footer">
+        <div class="container">
+
+            <div class="footer-nav">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-12">
+
+                        <h3>col title</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="#">menu</a></li>
+                            <li><a href="#">menu</a></li>
+                            <li><a href="#">menu</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-12">
+
+                        <h3>col title</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="#">menu</a></li>
+                            <li><a href="#">menu</a></li>
+                            <li><a href="#">menu</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-12">
+
+                        <h3>col title</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="#">menu</a></li>
+                            <li><a href="#">menu</a></li>
+                            <li><a href="#">menu</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-copyright">
+                &copy; YYYY TITRE DU SITE
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
+<!-- ======================================================================= -->
+<!-- FOOTER END                                                              -->
+<!-- ======================================================================= -->
---
+++ front-page.php
@@ -0,0 +1,31 @@
+<?php get_header() ?>
+
+<!-- ======================================================================= -->
+<!-- FRONT PAGE START                                                        -->
+<!-- ======================================================================= -->
+
+<div class="row">
+    <!-- Page Sidebar -->
+    <div class="col-xl-3 col-lg-4 col-sm-12">
+        <div class="page-sidebar">
+
+            Sidebar
+
+        </div>
+    </div>
+
+    <!-- Page Content -->
+    <div class="col-xl-9 col-lg-8 col-sm-12">
+        <div class="page-content">
+
+            Page content
+
+        </div>
+    </div>
+</div>
+
+<!-- ======================================================================= -->
+<!-- FRONT PAGE END                                                         -->
+<!-- ======================================================================= -->
+
+<?php get_footer() ?>
